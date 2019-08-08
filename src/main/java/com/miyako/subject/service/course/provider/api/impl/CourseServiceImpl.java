12ad04aec7cae9ce6bbf595cc23a6f508a512de3
lanxiao wei <weilanxiao@163.com>
2019-08-08 21:35:45 +0800

    user服务分页展示

--- src/main/java/com/miyako/subject/service/course/provider/api/impl/CourseServiceImpl.java
+++ src/main/java/com/miyako/subject/service/course/provider/api/impl/CourseServiceImpl.java
@@ -4,6 +4,8 @@ import com.alibaba.dubbo.config.annotation.Service;
 import com.miyako.subject.commons.domain.TbCourse;
 import com.miyako.subject.commons.mapper.TbCourseMapper;
 import com.miyako.subject.service.course.api.TbCourseService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
@@ -20,12 +22,18 @@ import java.util.List;
 @Component
 public class CourseServiceImpl implements TbCourseService{
 
+    private static Logger logger = LoggerFactory.getLogger(CourseServiceImpl.class);
+
     @Autowired
     private TbCourseMapper tbCourseMapper;
 
     @Override
     public List<TbCourse> selectAll(){
-        System.out.println("dubbo...");
+        logger.info("===>:CourseServiceImpl select all...");
         return tbCourseMapper.selectAll();
+    }
+
+    public void insert(TbCourse course){
+        tbCourseMapper.insertUseGeneratedKeys(course);
     }
 }
